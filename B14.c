c59b0f67758586462dee10c895e9523a8a13d85b
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-03-14 23:46:40 +0530

    Update B14.c

--- B14.c
+++ B14.c
@@ -10,23 +10,23 @@ Up to 10 km Fixed charge ₹80
 
 int main ()
 {
-    int distance, fare1, fare2, fare3;
+    int distance, fare;
     printf("provide distance travelled: ");
     scanf ("%d", &distance);
     if (distance<=10){
         printf("fare : Rs.80\n");
     }
     else if (distance>=11 && distance<=20){
-        fare1 = distance * 6;
-        printf("fare : %d\n", fare1); 
+        fare = distance * 6;
+        printf("fare : %d\n", fare); 
     }
     else if (distance >= 21 && distance <=30){
-        fare2= distance * 5;
-        printf("fare : %d\n", fare2);
+        fare= distance * 5;
+        printf("fare : %d\n", fare);
     }
     else if (distance >=31) {
-        fare3 = distance * 4;
-        printf("fare : %d\n", fare3);
+        fare = distance * 4;
+        printf("fare : %d\n", fare);
     }
     return 0;
 }
