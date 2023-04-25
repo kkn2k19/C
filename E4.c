2abe488846fdd2f669082e73d9d6d951a928ee19
cde_kkn <83773820+kkn2k19@users.noreply.github.com>
2023-04-25 22:32:25 +0530

    Add files via upload

--- E4.c
+++ E4.c
@@ -1,9 +1,31 @@
-/*
-4. Draw the triangle up to nth term
-1
-1 2 3
-1 2 3 4 5
-1 2 3 4 5 6 7
-1 2 3 4 5 6 7 8 9
-*/
 
+#include <stdio.h>
+
+int main() {
+   int n, i, j;
+
+   printf("Enter the number of lines required: ");
+   scanf("%d", &n);
+
+   for (i = 1; i <= n; i++) {
+      // print spaces before the numbers
+      for (j = 1; j <= n - i; j++) {
+         printf("  ");
+      }
+
+      // print numbers in increasing order
+      for (j = 1; j <= i; j++) {
+         printf("%d ", j);
+      }
+
+      // print numbers in decreasing order
+      for (j = i - 1; j >= 1; j--) {
+         printf("%d ", j);
+      }
+
+      // move to next line
+      printf("\n");
+   }
+
+   return 0;
+}
--- E6.c
+++ E6.c
@@ -1,3 +1,26 @@
-/**/
+#include<stdio.h>
 
-#include <stdio.h>
+int main()
+{
+printf("Enter the number of rows to show the star pattern:  ");
+int n, x,  y,  s = 1, k;
+scanf("%d",&n);
+for(x = 0; x <= n; x++)
+{
+for(y = n; y > x; y--)
+{
+printf(" ");
+}
+printf("*");
+if (x > 0)
+{
+for(k = 1; k <= s;  k++)
+{
+printf(" ");
+}
+s += 2;
+printf("*");
+}
+printf("\n");
+}
+}
