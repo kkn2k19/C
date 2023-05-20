57607433ca14da835472f6968c59e36eb9c38679
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-05-20 10:20:08 +0530

    Update E1.c

--- E1.c
+++ E1.c
@@ -8,15 +8,17 @@
 */
 
 #include <stdio.h>
-int main() {
-   int i, j, rows;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i) {
-      for (j = 1; j <= i; ++j) {
-         printf("%d ", j);
-      }
-      printf("\n");
-   }
-   return 0;
+
+int main()
+{
+    int i, j, n;
+    printf("Enter no. of rows : ");
+    scanf("%d", &n);
+    for (i=1; i<=n; i++){
+        for (j=1; j<=i; j++){
+           printf("%d ", j); 
+        }
+        printf("\n");
+    }
+    return 0;
 }
