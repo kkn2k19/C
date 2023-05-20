4c006b3f32a7ddc70ed41ca36925aa430499c14b
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-05-20 10:28:16 +0530

    Update E3.c

--- E3.c
+++ E3.c
@@ -9,13 +9,13 @@
 
 #include <stdio.h>
 int main() {
-   int rows, i, j, number = 1;
+   int n, i, j, a = 1;
    printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 1; i <= rows; i++) {
-      for (j = 1; j <= i; ++j) {
-         printf("%d ", number);
-         ++number;
+   scanf("%d", &n);
+   for (i = 1; i <= n; i++) {
+      for (j = 1; j <= i; j++) {
+         printf("%d ", a);
+         a++;
       }
       printf("\n");
    }
