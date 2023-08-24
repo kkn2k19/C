023cbb0716a9930c41bd790afc4e51cc3e18ca8f
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-24 07:09:30 +0530

    Update C9.c

--- C9.c
+++ C9.c
@@ -1,41 +1,42 @@
-/*Write a program in C to take a number & find all its PRIME FACTORS.
-Test Data :
-Enter a number: 36
-Expected Output :
-All Factors of 36 are =[2] [3][4] [6][9][12][18]
-Prime Factors of 36 are =[2] [3]*/
+/*
+Write a program in C to take a number & find all its PRIME FACTORS.
+*/
 
+#include <stdio.h>
+void prime_check(int n);
 
-# include <stdio.h>
-
-int main ()
+int main()
 {
-    int i, n, p;
-    printf("Provide a number : ");
-    scanf ("%d", &n);
-    printf("All Factors of %d are =", n);
-
-    for (i=1; i<=n; i++)
-    {
-        if (n%i==0)
-        {
-            printf("%d", i);
+    int n;
+    printf("Enter n : ");
+    scanf("%d", &n);
+    int i;
+    int arr[n];
+    printf("All Factors of %d are : ", n);
+    for (i=2; i<n; i++) {
+        if ((n%i)==0) {
+            printf("[%d] ", i);
         }
     }
-    printf("Prime Factors of %d are =", n);
-    for (i=1; i<n-1; i++)
-    {
-        if (n%i==0){
-            c=0;
+    printf("\nPrime Factors of %d are : ", n);
+    for (i=2; i<n; i++) {
+        if ((n%i)==0) {
+            prime_check(i);
         }
-    }
-    for (j=1; j<=i; j++){
-        if (i%j==0){
-            c++;
-        }
-    }
-    if (c==2){
-        printf("%d", i);
     }
     return 0;
+}
+
+void prime_check(int n) {
+    int i;
+    int flag=0;
+    for (i=2; i<n; i++) {
+        if ((n%i)==0) {
+            flag=1;
+            break;
+        }
+    }
+    if (flag==0) {
+        printf("[%d] ", n);
+    }
 }
